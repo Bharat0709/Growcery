9cb2bf571e32c069d85005b224ffd4869c4c19d9
Bharat Pahwa <pahwabharat15@gmail.com>
2023-11-27 20:45:06 +0530

    Add CategoriesNorder

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,6 +3,7 @@ const Order = require('../models/ordersmodel'); // Import the Order model
 const Item = require('../models/Itemsmodel');
 const user = require('../models/usermodel');
 const Cart = require('../models/cartmodel');
+const Address = require('../models/addressmodel');
 
 // Add item to cart
 exports.addtocart = async (req, res) => {
@@ -154,11 +155,17 @@ exports.getAllItemsInCart = async (req, res) => {
   const user = req.userId;
   const cart = await Cart.findOne({ user: user });
 
+  const totalPrice = cart.items.reduce((total, item) => total + item.price, 0);
+  const totalItems = cart.items.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
   if (!cart) {
     return res.status(404).json({ error: 'Cart not found' });
   }
 
-  res.status(200).json({ cart });
+  res.status(200).json({ cart, totalPrice, totalItems });
 };
 
 exports.deleteItemFromCart = async (req, res) => {
@@ -177,7 +184,7 @@ exports.deleteItemFromCart = async (req, res) => {
 
   if (cartItemIndex !== -1) {
     // Remove the item from the cart
-    const deletedItem = cart.items.splice(cartItemIndex, 1);
+    // const deletedItem = cart.items.splice(cartItemIndex, 1);
 
     // Update the cart's total price and total quantity
     cart.totalPrice = cart.items.reduce((total, item) => total + item.price, 0);
@@ -201,47 +208,79 @@ exports.deleteItemFromCart = async (req, res) => {
 };
 
 exports.placeOrder = async (req, res) => {
-  const user = req.userId; // Get user ID from JWT token
-
-  // Get the user's cart from the cache
-  const cart = cache.get(`cart:${user}`);
-
-  if (!cart || cart.items.length === 0) {
-    return res.status(400).json({ error: 'Empty cart' });
-  }
-
-  // Calculate the total price and total quantity
-  const totalPrice = cart.items.reduce(
-    (total, item) => total + item.quantity * item.itemDetails.price,
-    0
-  );
-  const totalQuantity = cart.items.reduce(
-    (total, item) => total + item.quantity,
-    0
-  );
-
-  // Create an order object based on the Order schema
-  const orderData = new Order({
-    user,
-    items: cart.items.map((cartItem) => ({
-      itemId: cartItem.itemId,
-      itemDetails: cartItem.itemDetails,
-      quantity: cartItem.quantity,
-    })),
-    totalPrice, // Add the total price to the order
-    totalQuantity, // Add the total quantity to the order
-    // Add other order details like shipping address, etc.
-  });
-
-  // Save the order data to the database
   try {
-    await orderData.save();
+    const user = req.userId;
+    console.log('user Id ', user); // Assuming you have a middleware to extract user ID from the request
+
+    // Retrieve user's cart with details
+    const cart = await Cart.findOne({ user: user });
+    console.log(cart);
+
+    if (!cart) {
+      return res.status(404).json({ error: 'Cart not found' });
+    }
+
+    // Retrieve user's selected address
+    const addresses = await Address.find({ userId: user });
+    const address = addresses.length > 0 ? addresses[0] : null;
+    console.log(address);
+
+    if (!address) {
+      return res.status(404).json({ error: 'Address not found' });
+    }
+
+    // Calculate total quantity and total price
+    const totalQuantity = cart.items.reduce(
+      (total, item) => total + item.quantity,
+      0
+    );
+    const totalPrice = cart.items.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+    // Retrieve detailed information for each item in the cart
+    const itemsDetails = await Promise.all(
+      cart.items.map(async (item) => {
+        const itemDetails = await Item.findById(item.itemId);
+        return {
+          itemId: item.itemId,
+          itemDetails: itemDetails.toObject(), // Convert to plain object for better serialization
+          quantity: item.quantity,
+        };
+      })
+    );
+
+    const order = new Order({
+      user: user,
+      cart: cart._id,
+      address: address._id,
+      items: cart.items.map((item) => ({
+        itemId: item.itemId._id,
+        // itemDetails: { name: item.itemId.name, price: item.itemId.price },
+        quantity: item.quantity,
+      })),
+      // Add other order details like total price, shipping address, order date, etc.
+    });
+
+    // Save the order
+    await order.save();
+
+    // You may want to clear the user's cart here
+    // await Cart.findOneAndUpdate({ user: userId }, { items: [] });
+
+    // Respond to the user with the order details
+    res.status(200).json({
+      message: 'Order created successfully',
+      order: {
+        orderId: order._id,
+        itemsDetails,
+        totalQuantity,
+        totalPrice,
+        address: address ? address.toObject() : null, // Convert to plain object for better serialization
+      },
+    });
   } catch (error) {
-    return res.status(500).json({ error: 'Error placing the order' });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
-
-  // Clear the user's cart in the cache
-  cache.del(`cart:${user}`);
-
-  res.status(200).json({ message: 'Order placed successfully', orderData });
 };
